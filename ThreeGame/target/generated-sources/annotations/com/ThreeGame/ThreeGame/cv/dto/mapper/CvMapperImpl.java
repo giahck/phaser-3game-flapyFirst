34ffcce467ae08e39a727fb2501fa90ca9b290ee
Gianluca <gianluca.giacchetta@gmail.com>
2024-09-02 22:30:14 +0200

    autenticazione google ok

--- ThreeGame/target/generated-sources/annotations/com/ThreeGame/ThreeGame/cv/dto/mapper/CvMapperImpl.java
+++ ThreeGame/target/generated-sources/annotations/com/ThreeGame/ThreeGame/cv/dto/mapper/CvMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-23T14:06:58+0200",
+    date = "2024-09-02T22:29:17+0200",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
 )
 @Component
